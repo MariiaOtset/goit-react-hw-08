80f1e5a3388aac2115e6364b36d99027dc48f06c
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-31 21:45:43 +0100

    another base url added for connection to backend

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+// axios.defaults.baseURL = "http://localhost-3000";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
